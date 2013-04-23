5242fdf61352ad8b5017878560e72043c54442b9
ferenctth8@gmail.com <ferenctth8@gmail.com@1555d126-ac9f-0956-fb73-3bd8a5abb38d>
2013-04-23 18:48:18 +0000

    All tests have been rewritten using the given-when-then scenario.

--- AuthorSOAPWS/test/edu/utcn/feritoth/dao/hibernate/AuthorHibernateDaoTest.java
+++ AuthorSOAPWS/test/edu/utcn/feritoth/dao/hibernate/AuthorHibernateDaoTest.java
@@ -32,76 +32,107 @@ public class AuthorHibernateDaoTest extends TestCase {
      * Test of getAllAuthors method, of class AuthorHibernateDao.
      */    
     public void testGetAllAuthors() {
+        //given 
         System.out.println("getAllAuthors");
         AuthorHibernateDao instance = new AuthorHibernateDao();
-        List expResult = instance.getAllAuthors();
-        List result = instance.getAllAuthors();
-        assertFalse(expResult.equals(result));        
+        
+        //when 
+        List<Author> result = instance.getAllAuthors();
+        
+        //then
+        assertEquals(result.size(), 42);
+        assertEquals("Herbert George Wells", result.get(40).getName());       
     }
 
     /**
      * Test of getMatchingAuthors method, of class AuthorHibernateDao.
      */    
     public void testGetMatchingAuthors() {
+        //given
         System.out.println("getMatchingAuthors");
         String nameSequence = "F";
         AuthorHibernateDao instance = new AuthorHibernateDao();
-        List expResult = instance.getMatchingAuthors(nameSequence);
-        List result = instance.getMatchingAuthors(nameSequence);
-        assertFalse(expResult.equals(result));        
+        
+        //when
+        List<Author> result = instance.getMatchingAuthors(nameSequence);
+        
+        //then
+        assertEquals(result.size(), 8);      
+        for (Author author:result){
+            System.out.println(author.toString() + "\n");        
+        }
     }
 
     /**
      * Test of findAuthorByID method, of class AuthorHibernateDao.
      */
     public void testFindAuthorByID() {
+        //given
         System.out.println("findAuthorByID");
         int authorID = 18;
         AuthorHibernateDao instance = new AuthorHibernateDao();
-        Author expResult = instance.findAuthorByID(authorID);
+        
+        //when
         Author result = instance.findAuthorByID(authorID);
-        assertFalse(expResult.equals(result));        
+        
+        //then
+        assertEquals("Maksim Gorky", result.getName());        
     }
 
     /**
      * Test of registerNewAuthor method, of class AuthorHibernateDao.
      */
-    /*public void testRegisterNewAuthor() {
-        System.out.println("registerNewAuthor");
-        String name = "Herbert George";
-        String surname = "Wells";
-        int birthYear = 1866;
-        int deathYear = 1946;
-        String shortDescription = "The founder of Science Fiction";
-        String referencePage = "http://hu.wikipedia.org/wiki/H._G._Wells";
+    public void testRegisterNewAuthor() {
+        //given
+        /*System.out.println("registerNewAuthor");        
+        String name = "James Fenimore";
+        String surname = "Cooper";
+        int birthYear = 1789;
+        int deathYear = 1851;
+        String shortDescription = "One of the most popular american novelists";
+        String referencePage = "http://en.wikipedia.org/wiki/James_Fenimore_Cooper";
         AuthorHibernateDao instance = new AuthorHibernateDao();
         boolean expResult = true;
+        
+        //when
         boolean result = instance.registerNewAuthor(name, surname, birthYear, deathYear, shortDescription, referencePage);
-        assertEquals(expResult, result);        
-    }*/
+        
+        //then
+        assertEquals(expResult, result);*/        
+    }
 
     /**
      * Test of updateExistingAuthor method, of class AuthorHibernateDao.
      */
     public void testUpdateExistingAuthor() {
-        System.out.println("updateExistingAuthor");
+        //given
+        /*System.out.println("updateExistingAuthor");
         int authorID = 31;
         String newDescription = "";
         AuthorHibernateDao instance = new AuthorHibernateDao();
         boolean expResult = true;
+        
+        //when        
         boolean result = instance.updateExistingAuthor(authorID, newDescription);
-        assertEquals(expResult, result);        
+        
+        //then
+        assertEquals(expResult, result);*/        
     }
 
     /**
      * Test of removeSelectedAuthor method, of class AuthorHibernateDao.
      */
-    /*public void testRemoveSelectedAuthor() {
-        System.out.println("removeSelectedAuthor");
-        int authorID = 42;
+    public void testRemoveSelectedAuthor() {
+        //given       
+        /*System.out.println("removeSelectedAuthor");
+        int authorID = 43;
         AuthorHibernateDao instance = new AuthorHibernateDao();
         boolean expResult = true;
+        
+        //when
         boolean result = instance.removeSelectedAuthor(authorID);
-        assertEquals(expResult, result);       
-    }*/
+        
+        //then 
+        assertEquals(expResult, result);*/       
+    }
 }
